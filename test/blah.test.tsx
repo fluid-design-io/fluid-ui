538e9a8d7f3445443993ee46f841c23071d50dba
Jiuzhen Pan <2216991777@qq.com>
2022-07-25 13:44:15 -0700

    fix(test): fixed test error
    Changed tsconfig --tsx to react

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Accordion } from '../src/lib';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<div />, div);
+    ReactDOM.render(<Accordion children={<p>HI</p>} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
