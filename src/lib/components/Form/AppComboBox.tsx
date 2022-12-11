caa08e4b01c7edb225a2a47b780cc96e8d1f93f6
Jiuzhen Pan <2216991777@qq.com>
2022-12-11 00:54:42 -0800

    feat(combobox): added multi-select, custom render functions, bug fixes and more

--- src/lib/components/Form/AppComboBox.tsx
+++ src/lib/components/Form/AppComboBox.tsx
@@ -1,10 +1,12 @@
 import { Combobox, Transition } from '@headlessui/react';
 import { useFormikContext } from 'formik';
 import React, { Fragment, useMemo, useState } from 'react';
-import { HiCheck, HiSelector } from 'react-icons/hi';
+import { HiCheck, HiOutlineX, HiSelector } from 'react-icons/hi';
 
 import clsxm from '../../helpers/clsxm';
 
+import { AnimatePresence, motion } from 'framer-motion';
+import { baseOptionClassName } from '.';
 import {
   ComboBoxComponent,
   ComboBoxProps,
@@ -25,26 +27,34 @@ const AppComboBox: ComboBoxComponent = React.forwardRef(
       label: rawLabel,
       list,
       listClassName,
+      inputClassName,
       buttonClassName,
       labelClassName,
+      itemClassName,
       listOptionSelectedClassName,
       listOptionActiveClassName,
       listOptionClassName,
       listOptionInactiveClassName,
-      inputClassName,
+      selectedItemsClassName,
       className,
       placeholder,
       description,
       disabled,
+      disabledKey,
       itemKey,
+      hasEmptyOption,
+      emptyOptionText,
+      emptyOptionValue,
+      multiple,
+      rednerOptionItem,
+      renderSelectedItem,
       ...props
     }: ComboBoxProps<C>,
     ref?: PolymorphicRef<C>
   ) => {
-    const Component = as || FormItem;
+    const Component = as || 'div';
     const { setFieldTouched, handleChange, errors, touched, values } =
       useFormikContext();
-    const [query, setQuery] = useState('');
     const [focused, setFocused] = useState(false);
     const { value, label, error } = useFormValue({
       rawName,
@@ -54,12 +64,22 @@ const AppComboBox: ComboBoxComponent = React.forwardRef(
       touched,
       placeholder,
     });
+    const getStringValue = (item: string | Record<string, any> | undefined) => {
+      if (typeof item === 'string') {
+        return item;
+      }
+      if (typeof item === 'object') {
+        return item[itemKey || 'name' || 'id'];
+      }
+      return '';
+    };
+    const [query, setQuery] = useState(getStringValue(value) || '');
     const filteredList = useMemo(() => {
       if (query === '') {
         return list;
       }
       return list.filter((item) => {
-        const itemValue = itemKey ? item[itemKey] : item;
+        const itemValue = getStringValue(item);
         return itemValue
           .toLowerCase()
           .replace(/\s/g, '')
@@ -67,7 +87,12 @@ const AppComboBox: ComboBoxComponent = React.forwardRef(
       });
     }, [query, list, itemKey]);
     const theme = useTheme().theme.form;
+
+    const userOnChange = props?.onChange,
+      userOnBlur = props?.onBlur,
+      userOnFocus = props?.onFocus;
     const theirProps = excludeClassName(props);
+
     return (
       <Combobox
         as={Component as any}
@@ -76,44 +101,172 @@ const AppComboBox: ComboBoxComponent = React.forwardRef(
         disabled={disabled}
         value={value}
         onChange={(e) => {
-          handleChange({ target: { type: 'text', name: rawName, value: e } });
+          handleChange({ target: { name: rawName, value: e } });
+          userOnChange && userOnChange(e);
+          !multiple && setQuery(getStringValue(e));
         }}
+        multiple={!!multiple as any}
         {...theirProps}
       >
         <Combobox.Label
           className={clsxm(
             'contrast:text-gray-90 text-sm font-medium text-gray-700 dark:text-gray-200',
-            props.className
+            labelClassName
           )}
         >
-          {label} {error && `(${error})`}
+          {label}
         </Combobox.Label>
 
-        <FormItem {...{ error, focused, description }}>
-          <div className='relative mt-1'>
-            <Combobox.Input
-              className={clsxm(theme.base, theme.select.button, inputClassName)}
-              onChange={(e) => setQuery(e.target.value)}
-              onFocus={() => (!focused ? setFocused(true) : undefined)}
-              value={query}
-              onBlur={() => {
-                setFieldTouched(rawName);
-                setFocused(false);
-              }}
-            />
-            <Combobox.Button
+        <FormItem {...{ error, descriptionError: error, focused, description }}>
+          <div className={clsxm(theme.base, 'relative mt-1', itemClassName)}>
+            <div
               className={clsxm(
-                'absolute inset-y-0 right-0 flex items-center pr-2 hocus:opacity-80',
-                buttonClassName
+                multiple && value?.length > 0 && selectedItemsClassName
               )}
             >
-              {sr && <span className='sr-only'>{sr}</span>}
-              <HiSelector
-                aria-hidden='true'
-                className='h-5 w-5 text-gray-400'
+              {renderSelectedItem &&
+                multiple &&
+                value?.length > 0 &&
+                value.map((item: any) => (
+                  <Fragment key={`${rawName}-${getStringValue(item)}-selected`}>
+                    {renderSelectedItem({
+                      item,
+                      remove: () =>
+                        handleChange({
+                          target: {
+                            name: rawName,
+                            value: value.filter((v: any) => v !== item) as any,
+                          },
+                        }),
+                    })}
+                  </Fragment>
+                ))}
+              <AnimatePresence>
+                {!renderSelectedItem && multiple && value?.length > 0 && (
+                  <motion.div
+                    initial={{
+                      opacity: 0,
+                      height: 0,
+                    }}
+                    animate={{
+                      opacity: 1,
+                      height: 'auto',
+                    }}
+                    exit={{
+                      opacity: 0,
+                      height: 0,
+                    }}
+                    transition={{
+                      type: 'spring',
+                      bounce: 0,
+                      duration: 0.38,
+                    }}
+                  >
+                    <div
+                      className={clsxm(
+                        multiple && 'flex flex-wrap gap-2',
+                        multiple && value?.length > 0 && 'p-2'
+                      )}
+                    >
+                      <AnimatePresence mode='popLayout'>
+                        {value.map((v: any) => (
+                          <motion.div
+                            initial={{
+                              opacity: 0,
+                              scale: 0.92,
+                              filter: 'blur(0px)',
+                            }}
+                            animate={{
+                              opacity: 1,
+                              scale: 1,
+                              filter: 'blur(0px)',
+                            }}
+                            exit={{
+                              opacity: 0,
+                              filter: 'blur(5px)',
+                              transition: { duration: 0.15, delay: 0.05 },
+                            }}
+                            transition={{
+                              type: 'spring',
+                              bounce: 0,
+                              duration: 0.25,
+                            }}
+                            key={`${rawName}-selected-${getStringValue(v)}`}
+                            layoutId={`${rawName}-selected-${getStringValue(
+                              v
+                            )}`}
+                          >
+                            <Button
+                              as={motion.button}
+                              weight='light'
+                              color='gray'
+                              size='xs'
+                              label={getStringValue(v)}
+                              iconEnd={HiOutlineX}
+                              iconClassName='text-gray-400 dark:text-gray-600'
+                              onClick={() => {
+                                const newValue = value.filter(
+                                  (item: any) => item !== v
+                                ) as any;
+                                handleChange({
+                                  target: {
+                                    type: 'text',
+                                    name: rawName,
+                                    value: newValue,
+                                  },
+                                });
+                              }}
+                            />
+                          </motion.div>
+                        ))}
+                      </AnimatePresence>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+            <div
+              className={clsxm(
+                'relative w-full',
+                multiple &&
+                  value &&
+                  value?.length > 0 &&
+                  'border-t border-t-gray-200 dark:border-t-gray-700'
+              )}
+            >
+              <Combobox.Input
+                className={clsxm(
+                  'bg-transparent outline-none focus:outline-none',
+                  theme.select.button,
+                  inputClassName
+                )}
+                onChange={(e) => setQuery(e.target.value)}
+                onFocus={() => (!focused ? setFocused(true) : undefined)}
+                value={query}
+                placeholder={placeholder || 'Type to search...'}
+                onBlur={() => {
+                  setFieldTouched(rawName);
+                  setFocused(false);
+                }}
               />
-            </Combobox.Button>
-
+              <Combobox.Button
+                className={clsxm(
+                  'absolute inset-y-0 right-0 flex items-center pr-2 hocus:opacity-80',
+                  'rtl:left-0 rtl:right-auto rtl:pr-0 rtl:pl-2',
+                  buttonClassName
+                )}
+                onBlur={() => {
+                  setFieldTouched(rawName);
+                  setFocused(false);
+                }}
+              >
+                {sr && <span className='sr-only'>{sr}</span>}
+                <HiSelector
+                  aria-hidden='true'
+                  className='h-5 w-5 text-gray-400'
+                />
+              </Combobox.Button>
+            </div>
             <Transition
               as={Fragment}
               leave='transition ease-in duration-100'
@@ -125,23 +278,53 @@ const AppComboBox: ComboBoxComponent = React.forwardRef(
                   className={clsxm(
                     theme.popover,
                     'absolute overflow-auto mt-2 py-1 sm:text-sm',
+                    multiple && 'top-full',
                     listClassName
                   )}
+                  onBlur={() => {
+                    setFocused(false);
+                    userOnBlur && userOnBlur();
+                    // eslint-disable-next-line no-undef
+                    setTimeout(() => {
+                      setFieldTouched(rawName);
+                    }, 300);
+                  }}
+                  onFocus={() => {
+                    setFocused(true);
+                    userOnFocus && userOnFocus();
+                  }}
                 >
+                  {hasEmptyOption && (
+                    <Combobox.Option
+                      as={Button}
+                      innerAs='li'
+                      key={`${rawName}-empty`}
+                      shape='square'
+                      value={emptyOptionValue || ''}
+                      className={clsxm(baseOptionClassName)}
+                    >
+                      <span className='block truncate'>
+                        {emptyOptionText || 'None'}
+                      </span>
+                    </Combobox.Option>
+                  )}
+
+                  {rednerOptionItem &&
+                    list.map((item, i) => (
+                      <Fragment key={`${rawName}-${i}`}>
+                        {rednerOptionItem({ item, Option: Combobox.Option })}
+                      </Fragment>
+                    ))}
                   {filteredList.map((item) => (
                     <Combobox.Option
                       as={Button}
                       innerAs='li'
-                      key={`${rawName}-${itemKey ? item[itemKey] : item}`}
+                      key={`${rawName}-${getStringValue(item)}`}
                       shape='square'
-                      value={item[itemKey]}
+                      value={item}
                       className={({ active, selected }) =>
                         clsxm(
-                          'flex w-full items-center justify-start !border-x-transparent select-none',
-                          'ui-selected:btn-light-primary dark:ui-selected:btn-bold-primary',
-                          'ui-not-selected:ui-active:btn-light-primary',
-                          'dark:ui-not-selected:ui-active:btn-bold-primary',
-                          'dark:ui-not-selected:ui-not-active:text-gray-200',
+                          baseOptionClassName,
                           selected
                             ? [listOptionSelectedClassName]
                             : active
@@ -150,16 +333,22 @@ const AppComboBox: ComboBoxComponent = React.forwardRef(
                           listOptionClassName
                         )
                       }
+                      disabled={
+                        (disabledKey && item[disabledKey]) ||
+                        (typeof multiple === 'number' &&
+                          value.length >= multiple &&
+                          !value.includes(item))
+                      }
                     >
                       {({ active, selected }) => (
                         <>
                           <span
                             className={clsxm(
                               'block truncate',
-                              selected && 'font-semibold'
+                              selected && 'font-medium'
                             )}
                           >
-                            {item[itemKey]}
+                            {getStringValue(item)}
                           </span>
 
                           {selected && (
